b963ec58e3d62c2ff0b1820fa9eff7f8be71d3bb
Max Trubetskoy <handcrab@gmail.com>
2015-02-24 16:57:44 +0300

    create image scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224133811) do
+ActiveRecord::Schema.define(version: 20150224135717) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "images", force: :cascade do |t|
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
