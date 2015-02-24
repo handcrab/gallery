08b10dcdea25bf967a78e16317eb38b574e0e041
Max Trubetskoy <handcrab@gmail.com>
2015-02-24 16:42:34 +0300

    create album scaffold

---
+++ app/views/albums/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @album, :id, :title, :description, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224125910) do
+ActiveRecord::Schema.define(version: 20150224133811) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "pages", force: :cascade do |t|
     t.string   "title",      null: false
