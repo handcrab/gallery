b499357ea30529269004108af1afb276a6ebe5b3
Max Trubetskoy <handcrab@gmail.com>
2015-02-25 14:20:08 +0300

    change picture size presets

--- README.md
+++ README.md
@@ -2,13 +2,14 @@ Dummy gallery rails app #learning
 
 # Features
 - Static pages with seo names
-- Admin can create galleries with multiple images
+- User can create galleries with multiple images
   - Add image fields dynamically
 
 # TODO  
 - Admin
 - Multilpe-file upload
 - Sortable images
+- ? Allow to see all images on albums page
 
 # Setup
 `sudo apt-get install -y imagemagick`
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
   has_and_belongs_to_many :albums
 
-  has_attached_file :picture, styles: { large: "600x600>", medium: '300x300', thumb: "100x100>" } 
+  has_attached_file :picture, styles: { large: "900x900>", medium: '300x300', thumb: "100x100#" } 
   #, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
   validates :picture, presence: true
